2369ed698dc6a8efa6be018162dfb122ba0abc0f
Austin Vershel <avershel@gmail.com>
2016-10-03 11:02:48 -0400

    Initial Commit

---
+++ Travel Ball/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Travel Ball
+//
+//  Created by Austin Vershel on 10/3/16.
+//  Copyright © 2016 Austin Vershel. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
